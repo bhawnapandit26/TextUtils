a08af2a23a3ef97f24b0927fe3f1b896d9932e51
Bhawna Pandit <bhawnapandit26@gmail.com>
2023-10-09 00:40:34 +0530

    commit throught cmd

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <>
         <header className="w-full text-gray-700 bg-white border-b border-gray-200 shadow-sm body-font">
         <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row justify-center">    
-            <div className='font-bold text-3xl'>
+            <div className='font-semibold text-3xl'>
                 Text Utils  
             </div>    
         </div>
